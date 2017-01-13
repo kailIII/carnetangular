ac6be61102ab45b1016bac7c1528a988e9672b7e
nachmo5 <moham3d.nachit@gmail.com>
2017-01-13 01:02:56 +0000

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carnetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the carnetApp
+ */
+angular.module('carnetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carnetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the carnetApp
+ */
+angular.module('carnetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
